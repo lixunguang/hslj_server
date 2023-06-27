254d779baadc99e35ce106b3e790b29b887fbbe6
lixunguang <29397924@qq.com>
2023-06-27 18:36:35 +0800

    规范化

--- internal/controller/location_controller.go
+++ internal/controller/location_controller.go
@@ -2,25 +2,33 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"hslj/internal/dto"
+	"hslj/pkg/cerror"
+	"hslj/pkg/logger"
 	"hslj/pkg/util"
 )
 
-type LocationRes struct {
-	Name      string
-	Desc      string
-	Latitude  float64
-	Longitude float64
-	Rating    int
-	//OpenTime time.Time
-	//CloseTime time.Time
-	//UserReviews []int
-
-}
-
 func GetLocationList(ctx *gin.Context) {
-	var res []LocationRes
+	logger.Infoc(ctx, "[%s] start***", "GetLocationList Controller")
 
-	var item LocationRes
+	// 获取参数
+	var param dto.GetLocationListParam
+	if err := ctx.ShouldBindJSON(&param); err != nil {
+		logger.Errorc(ctx, "[%s] bind params fail,err=%+v", "method", err)
+		util.FailJson(ctx, cerror.InvalidParams)
+		return
+	}
+	if util.IsDebug() {
+		logger.Infoc(ctx, "[%s] input param: %+v", "Login Controller", param)
+	}
+
+	//参数校验
+
+	//调用service
+
+	var res []dto.GetLocationListRes
+
+	var item dto.GetLocationListRes
 	item.Name = "北京颐和园"
 	item.Desc = "颐和园描述。。。"
 	item.Latitude = 1.11
@@ -37,12 +45,42 @@ func GetLocationList(ctx *gin.Context) {
 	item.Rating = 4
 	res = append(res, item)
 
-	util.SuccessJson(ctx, res)
+	var err cerror.Cerror = nil
+	//结果返回
+	if err == nil {
+		util.SuccessJson(ctx, res)
+	} else {
+		util.FailJson(ctx, err)
+	}
+
+	logger.Infoc(ctx, "[%s] end***  result:  res=%+v,token =%+v", "GetLocationList Controller", res, "tokenStr")
 }
 
 func AddLocation(ctx *gin.Context) {
+	logger.Infoc(ctx, "[%s] start***", "AddLocation Controller")
+	// 获取参数
+	var param dto.AddLocationParam
+	if err := ctx.ShouldBindJSON(&param); err != nil {
+		logger.Errorc(ctx, "[%s] bind params fail,err=%+v", "method", err)
+		util.FailJson(ctx, cerror.InvalidParams)
+		return
+	}
+	if util.IsDebug() {
+		logger.Infoc(ctx, "[%s] input param: %+v", "Login Controller", param)
+	}
+	//参数校验
 
-	res := "addok!"
+	//调用service
+	var res dto.AddLocationRes
 
-	util.SuccessJson(ctx, res)
+	var err cerror.Cerror = nil
+
+	//结果返回
+	if err == nil {
+		util.SuccessJson(ctx, res)
+	} else {
+		util.FailJson(ctx, err)
+	}
+
+	logger.Infoc(ctx, "[%s] end***  result:  res=%+v,token =%+v", "AddLocation Controller", res, "tokenStr")
 }
--- internal/dto/location_dto.go
+++ internal/dto/location_dto.go
@@ -1,1 +1,27 @@
 package dto
+
+type GetLocationListParam struct {
+	LocationCode int `json:"location_code"`
+}
+
+type GetLocationListRes struct {
+	Name      string  `json:"name"`
+	Desc      string  `json:"desc"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	Rating    int     `json:"rating"`
+	//OpenTime time.Time
+	//CloseTime time.Time
+	//UserReviews []int
+}
+
+type AddLocationParam struct {
+	Name string `json:"name"`
+	Desc string `json:"desc"`
+}
+
+type AddLocationRes struct {
+}
+
+type LocationRes struct {
+}
